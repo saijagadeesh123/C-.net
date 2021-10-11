0ec6c002a1db30963b7fd8b61feb6ba9aa43d7ad
Your Name <you@example.com>
2021-10-11 16:47:39 +0530

    FactoryDesignPatterns

---
+++ FactoryDesignPattern/FactoryDesignPattern/CreditCard.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignPattern
+{
+    //simple factory design pattern-in which we cannot create a new product and it is tightly coupled 
+    public interface CreditCard
+    {
+        string GetCardType();
+        int GetCreditLimit();
+        int GetAnnualCharge();
+    }
+    class MoneyBack : CreditCard
+    {
+        public string GetCardType()
+        {
+            return "MoneyBack";
+        }
+        public int GetCreditLimit()
+        {
+            return 30000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 300;
+        }
+    }
+    class Titanium : CreditCard
+    {
+        public string GetCardType()
+        {
+            return "MoneyBack";
+        }
+        public int GetCreditLimit()
+        {
+            return 25000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 400;
+        }
+    }
+    class Platinum : CreditCard
+    {
+        public string GetCardType()
+        {
+            return "platinum";
+        }
+        public int GetCreditLimit()
+        {
+            return 35000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 500;
+        }
+    }
+    class CreditCardFactory
+    {
+        public static CreditCard GetCreditCard(string cardType)
+        {
+            CreditCard cardDetails = null;
+            if (cardType == "MoneyBack")
+            {
+                cardDetails = new MoneyBack();
+            }
+            else if (cardType == "Titanium")
+            {
+                cardDetails = new Titanium();
+            }
+            else if (cardType == "Platinum")
+            {
+                cardDetails = new Platinum();
+            }
+            return cardDetails;
+        }
+    }
+    //method factory design pattern
+    public abstract class CreditCardFactory1
+    {
+        protected abstract CreditCard MakeProduct();
+        public CreditCard CreateProduct()
+        {
+            return this.MakeProduct();
+        }
+    }
+    public class MoneyBackFactory : CreditCardFactory1
+    {
+        protected override CreditCard MakeProduct()
+        {
+            CreditCard product = new MoneyBack();
+            return product;
+        }
+    }
+    public class PlatinumFactory : CreditCardFactory1
+    {
+        protected override CreditCard MakeProduct()
+        {
+            CreditCard product = new Platinum();
+            return product;
+        }
+    }
+    public class TitaniumFactory : CreditCardFactory1
+    {
+        protected override CreditCard MakeProduct()
+        {
+            CreditCard product = new Titanium();
+            return product;
+        }
+    }
+
+}
---
+++ FactoryDesignPattern/FactoryDesignPattern/factorydesign.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FactoryDesignPattern
+{
+    class factorydesign
+    {
+        static void Main(string[] args)
+        {
+            CreditCard cardDetails = CreditCardFactory.GetCreditCard("Platinum");
+
+            if (cardDetails != null)
+            {
+                Console.WriteLine("CardType : " + cardDetails.GetCardType());
+                Console.WriteLine("CreditLimit : " + cardDetails.GetCreditLimit());
+                Console.WriteLine("AnnualCharge :" + cardDetails.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type");
+            }
+            CreditCard creditCard = new TitaniumFactory().CreateProduct();
+            if (creditCard != null)
+            {
+                Console.WriteLine("Card Type : " + creditCard.GetCardType());
+                Console.WriteLine("Credit Limit : " + creditCard.GetCreditLimit());
+                Console.WriteLine("Annual Charge :" + creditCard.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type");
+            }
+        }
+    }
+}
